1cf8cead233ec120e86c614f08df1c346c963679
Roman Skrylnyk <romam65@outlook.com>
2023-01-11 19:42:05 +0200

    Create setupmymachine.ps1
    Version 1.1

---
+++ setupmymachine.ps1
@@ -0,0 +1,20 @@
+mkdir c:\github
+winget install -e --id Git.Git --accept-package-agreements --accept-source-agreements
+winget install -e --id Google.Chrome
+winget install -e --id SlackTechnologies.Slack
+winget install -e --id Notepad++.Notepad++
+winget install -e --id Adobe.Acrobat.Reader.64-bit
+winget install -e --id Dropbox.Dropbox
+winget install -e --id Oracle.JavaRuntimeEnvironment
+winget install -e --id TeamViewer.TeamViewer
+winget install -e --id AnyDeskSoftwareGmbH.AnyDesk
+winget install -e --id 7zip.7zip
+winget install -e --id Valve.Steam
+winget install -e --id Telegram.TelegramDesktop
+winget install -e --id GitHub.GitHubDesktop
+winget install -e --id VideoLAN.VLC
+winget install -e --id NickeManarin.ScreenToGif
+winget install -e --id Microsoft.RemoteDesktopClient
+winget install -e --id OBSProject.OBSStudio
+winget install -e --id Microsoft.PowerShell
+winget install -e --id Greenshot.Greenshot
