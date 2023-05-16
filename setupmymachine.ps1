1883f0ce614ac634f76ea4461eef12f54fd590e6
Roman Skrylnyk <roman.skrylnyk@ukad-group.com>
2023-05-16 17:15:59 +0300

    Added OperaGX

--- setupmymachine.ps1
+++ setupmymachine.ps1
@@ -20,4 +20,5 @@ winget install -e --id OBSProject.OBSStudio
 winget install -e --id Skillbrains.Lightshot
 winget install -e --id zokugun.MrCode
 winget install -e --id=Zoom.Zoom
-winget install --id=Spotify.Spotify -v "1.2.11.916.geb595a67" -e 
+winget install --id=Spotify.Spotify -v "1.2.11.916.geb595a67" -e
+winget install -e --id=Opera.OperaGX
